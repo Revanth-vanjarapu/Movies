d6b947b5bdc0cc0c92d85fb82ce842d611227b50
Revanth <46972569+Revanth-vanjarapu@users.noreply.github.com>
2025-11-12 23:08:22 +0530

    Refactor Pagination component for cleaner syntax

--- src/components/Pagination/index.js
+++ src/components/Pagination/index.js
@@ -1,50 +1,50 @@
-import React from 'react';
-import './index.css';
+import React from 'react'
+import './index.css'
 
 class Pagination extends React.Component {
   state = {
     pageNo: 1,
-  };
+  }
 
   onNextPage = () => {
-    const { apiCallback, totalPages } = this.props;
+    const { apiCallback, totalPages} = this.props
     this.setState(
-      (prevState) => {
+      prevState => {
         if (prevState.pageNo < totalPages) {
           return {
             pageNo: prevState.pageNo + 1,
-          };
+          }
         }
-        return prevState;
+        return prevState
       },
       () => {
-        const { pageNo } = this.state;
-        apiCallback(pageNo);
+        const { pageNo} = this.state
+        apiCallback(pageNo)
       },
-    );
-  };
+    )
+  }
 
   onPrevPage = () => {
-    const { apiCallback } = this.props;
+    const { apiCallback} = this.props
     this.setState(
-      (prevState) => {
+      prevState => {
         if (prevState.pageNo > 1) {
           return {
             pageNo: prevState.pageNo - 1,
-          };
+          }
         }
-        return prevState;
+        return prevState
       },
       () => {
-        const { pageNo } = this.state;
-        apiCallback(pageNo);
+        const { pageNo} = this.state
+        apiCallback(pageNo)
       },
-    );
-  };
+    )
+  }
 
   render() {
-    const { pageNo } = this.state;
-    const { totalPages } = this.props;
+    const { pageNo} = this.state
+    const { totalPages} = this.props
 
     return (
       <div className="pagenation">
@@ -68,8 +68,8 @@ class Pagination extends React.Component {
           Next
         </button>
       </div>
-    );
+    )
   }
 }
 
-export default Pagination;
+export default Pagination
