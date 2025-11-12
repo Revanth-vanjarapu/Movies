36e96077e44371ac8f5a9f160b1c9b97d97809d5
Revanth <46972569+Revanth-vanjarapu@users.noreply.github.com>
2025-11-12 22:01:39 +0530

    Refactor Pagination component for code consistency

--- src/components/Pagination/index.js
+++ src/components/Pagination/index.js
@@ -1,50 +1,50 @@
-import React from "react"
-import "./index.css"
+import React from 'react';
+import './index.css';
 
 class Pagination extends React.Component {
   state = {
     pageNo: 1,
-  }
+  };
 
   onNextPage = () => {
-    const { apiCallback, totalPages } = this.props
+    const {apiCallback, totalPages} = this.props;
     this.setState(
       (prevState) => {
         if (prevState.pageNo < totalPages) {
           return {
             pageNo: prevState.pageNo + 1,
-          }
+          };
         }
-        return prevState
+        return prevState;
       },
       () => {
-        const { pageNo } = this.state
-        apiCallback(pageNo)
-      }
-    )
-  }
+        const {pageNo} = this.state;
+        apiCallback(pageNo);
+      },
+    );
+  };
 
   onPrevPage = () => {
-    const { apiCallback } = this.props
+    const {apiCallback} = this.props;
     this.setState(
       (prevState) => {
         if (prevState.pageNo > 1) {
           return {
             pageNo: prevState.pageNo - 1,
-          }
+          };
         }
-        return prevState
+        return prevState;
       },
       () => {
-        const { pageNo } = this.state
-        apiCallback(pageNo)
-      }
-    )
-  }
+        const {pageNo} = this.state;
+        apiCallback(pageNo);
+      },
+    );
+  };
 
   render() {
-    const { pageNo } = this.state
-    const { totalPages } = this.props
+    const {pageNo} = this.state;
+    const {totalPages} = this.props;
 
     return (
       <div className="pagenation">
@@ -52,8 +52,7 @@ class Pagination extends React.Component {
           type="button"
           className="btn btn-primary"
           onClick={this.onPrevPage}
-          disabled={pageNo === 1}
-        >
+          disabled={pageNo === 1}>
           Prev
         </button>
         <p className="page-no">{pageNo} </p>
@@ -63,13 +62,12 @@ class Pagination extends React.Component {
           name="Next"
           title="Next"
           className="btn btn-primary"
-          onClick={this.onNextPage}
-        >
+          onClick={this.onNextPage}>
           Next
         </button>
       </div>
-    )
+    );
   }
 }
 
-export default Pagination
+export default Pagination;
